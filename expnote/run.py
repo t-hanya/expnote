1d469f46a08c844121e45bd80a6bd36b944aadb0
t-hanya <toshi.hanya@gmail.com>
2022-09-25 19:29:51 +0900

    Add RunGroup class

--- expnote/run.py
+++ expnote/run.py
@@ -5,12 +5,25 @@ A run data structure.
 
 from dataclasses import dataclass
 from typing import Optional
+from typing import List
+from typing import Tuple
 
 
 @dataclass
 class Run:
     """A run data structure."""
     id: str
+    params: dict
+    metrics: dict
+    step_metrics: Optional[list] = None
+
+
+@dataclass
+class RunGroup:
+    """A run group data structure."""
+
+    runs: List[Run]
+    id: Tuple[str, ...]
     params: dict
     metrics: dict
     step_metrics: Optional[list] = None
--- tests/test_run.py
+++ tests/test_run.py
@@ -1,4 +1,5 @@
 from expnote.run import Run
+from expnote.run import RunGroup
 
 
 class TestRun:
@@ -8,3 +9,18 @@ class TestRun:
         assert run.id == '0'
         assert run.params == {'lr': 0.5}
         assert run.metrics == {'acc': 0.9}
+
+
+class TestRunGroup:
+
+    def test(self):
+        run1 = Run(id='0', params={'lr': 0.5}, metrics={'acc': 0.9})
+        run2 = Run(id='1', params={'lr': 0.5}, metrics={'acc': 0.8})
+        group = RunGroup([run1, run2],
+                         id=('0', '1'),
+                         params={'lr': 0.5},
+                         metrics={'acc': 0.85})
+
+        assert group.id == ('0', '1')
+        assert group.params == {'lr': 0.5}  # common param
+        assert group.metrics == {'acc': 0.85}  # averaged

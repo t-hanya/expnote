ca640c7ab587039134c8d92473c01899f3b2caf5
t-hanya <toshi.hanya@gmail.com>
2022-11-05 00:34:51 +0900

    Add table editting and styling features

--- expnote/note.py
+++ expnote/note.py
@@ -3,36 +3,183 @@ Data structures to represent the contents of an experiment notebook.
 """
 
 
+import base64
 from dataclasses import dataclass
-from typing import List
+import io
 from typing import Any
+from typing import List
+from typing import Literal
 from typing import Optional
+from typing import Tuple
+from typing import Union
 
 from PIL import Image
 
 
+TableValue = Union[None, int, float, str, bool, Image.Image]
+
+
 @dataclass
+class TableCellStyle:
+    """Table cell style data."""
+    text_color: Optional[Literal['red', 'green']] = None
+
+
+@dataclass
+class TableCell:
+    """Table cell data."""
+    value: TableValue
+    style: TableCellStyle
+
+
 class Table:
     """A table data structure."""
-    columns: List[str]
-    rows: List[List[Any]]
-    note: Optional[str] = None
-    title: Optional[str] = None
 
-    def __str__(self) -> str:
-        # table
+    def __init__(self,
+                 columns: List[str],
+                 rows: List[List[TableValue]],
+                 note: Optional[str] = None,
+                 title: Optional[str] = None,
+                 cell_styles: Optional[List[List[dict]]] = None
+                ) -> None:
+        self.columns = columns
+        self._data = []
+        for i, row_values in enumerate(rows):
+            if cell_styles is not None:
+                row_styles = cell_styles[i]
+            else:
+                row_styles = [{} for _ in row_values]
+            self._data.append([TableCell(value=value,
+                                         style=TableCellStyle(**style))
+                               for value, style in zip(row_values, row_styles)])
+        self.title = title
+        self.note = note
+
+    @property
+    def rows(self) -> List[List[TableValue]]:
+        rows = []
+        for row_data in self._data:
+            rows.append([data.value for data in row_data])
+        return rows
+
+    def __getitem__(self, key: Tuple[int, Union[int, str]]) -> TableCell:
+        """Access to table cell data with `table[row_idx, col_idx_or_name]`."""
+        if not type(key) == tuple or len(key) != 2:
+            raise KeyError(
+                'Use `table[row_idx, col_idx_or_name]` syntax.')
+        row_idx = int(key[0])
+        col_idx_or_name = key[1]
+        if type(col_idx_or_name) == str:
+            col_idx = self.columns.index(col_idx_or_name)
+        else:
+            col_idx = int(col_idx_or_name)
+        return self._data[row_idx][col_idx]
+
+    def __setitem__(self,
+                    key: Tuple[int, Union[int, str]],
+                    value: TableValue,
+                   ) -> None:
+        """Set cell data with `table[row_idx, col_idx_or_name] = value`."""
+        _ = int(key[0])  # type check
+        col_idx_or_name = key[1]
+        if type(col_idx_or_name) == str:
+            name = col_idx_or_name
+            if not name in self.columns:
+                self.append_column(name, [None for _ in range(len(self._data))])
+        self[key].value = value
+
+    def insert_row(self, index: int, values: List[TableCell]) -> None:
+        """Insert the row to the table."""
+        row_data = [TableCell(value=value, style=TableCellStyle())
+                    for value in values]
+        self._data.insert(index, row_data)
+
+    def append_row(self, values: List[TableCell]) -> None:
+        """Append the row to the table."""
+        row_data = [TableCell(value=value, style=TableCellStyle())
+                    for value in values]
+        self._data.append(row_data)
+
+    def insert_column(self,
+                      index: int,
+                      name: str,
+                      values: List[TableValue]
+                     ) -> None:
+        """Insert the column to the table."""
+        column_data = [TableCell(value=value, style=TableCellStyle())
+                       for value in values]
+        self.columns.insert(index, name)
+        for row_data, new_data in zip(self._data, column_data):
+            row_data.insert(index, new_data)
+
+    def append_column(self,
+                      name: str,
+                      values: List[TableValue]
+                     ) -> None:
+        """Append the column to the table."""
+        column_data = [TableCell(value=value, style=TableCellStyle())
+                       for value in values]
+        self.columns.append(name)
+        for row_data, new_data in zip(self._data, column_data):
+            row_data.append(new_data)
+
+    def _repr_html_(self) -> str:
+        html = '<table>\n'
+        html += '  <tr>{}</tr>\n'.format(
+            ''.join([f'<th>{name}</th>' for name in self.columns]))
+        for row_data in self._data:
+            html += '  <tr>'
+            for data in row_data:
+                styles = []
+                if data.style.text_color is not None:
+                    styles.append('color:{}'.format(data.style.text_color))
+                if styles:
+                    style = ' style="{}"'.format(';'.join(styles))
+                else:
+                    style = ''
+                if isinstance(data.value, Image.Image):
+                    value = _to_html_img(data.value)
+                else:
+                    value = data.value
+                html += '<td{}>{}</td>'.format(style, value)
+            html += '</tr>\n'
+        html += '</table>'
+        return html
+
+    def _to_markdown_table(self, pretty: bool = False) -> str:
+        code_color = {'red': '\33[31m', 'green': '\33[32m'}
+        code_end = '\33[0m'
+
+        rows = []
+        styles = []
+        for row_data in self._data:
+            rows.append([])
+            styles.append([])
+            for data in row_data:
+                if isinstance(data.value, Image.Image):
+                    rows[-1].append('(image)')
+                else:
+                    rows[-1].append(str(data.value))
+                styles[-1].append(data.style)
+
         width_arr = []
         for i in range(len(self.columns)):
-            contents = [r[i] for r in self.rows] + [self.columns[i]]
+            contents = [r[i] for r in rows] + [self.columns[i]]
             max_len = max([len(str(v)) for v in contents])
             width_arr.append(max_len)
+
         lines = []
-        lines.append(' ' + ' | '.join([col.ljust(w) for col, w
-                                       in zip(self.columns, width_arr)]))
-        lines.append('-' + '-+-'.join(['-' * w for w in width_arr]) + '-')
-        for row in self.rows:
-            lines.append(' ' + ' | '.join([str(v).ljust(w) for v, w
-                                           in zip(row, width_arr)]))
+        lines.append('| ' + ' | '.join([col.ljust(w) for col, w
+                                       in zip(self.columns, width_arr)]) + ' |')
+        lines.append('|-' + '-|-'.join(['-' * w for w in width_arr]) + '-|')
+        for row, row_style in zip(rows, styles):
+            line_elems = []
+            for value, style, width in zip(row, row_style, width_arr):
+                text = value.ljust(width)
+                if pretty and style.text_color is not None:
+                    text = code_color[style.text_color] + text + code_end
+                line_elems.append(text)
+            lines.append('| ' + ' | '.join([text for text in line_elems]) + ' |')
         content = '\n'.join(lines)
 
         # title & note
@@ -41,6 +188,35 @@ class Table:
         if self.note is not None:
             content = content + '\n\n' + self.note
         return content
+
+    def _repr_pretty_(self, *args) -> str:
+        return self._to_markdown_table(pretty=True)
+
+    def _repr_markdown_(self) -> str:
+        return self._to_markdown_table()
+
+    def __str__(self) -> str:
+        return self._repr_markdown_()
+
+
+def _to_html_img(image: Image.Image,
+                 max_width: int = 150,
+                 max_height: int = 100) -> str:
+    if (image.height / image.width) > (max_height / max_width):
+        if image.height > max_height:
+            width = int(round(image.width * max_height / image.height))
+            height = max_height
+            image = image.resize((width, height), resample=Image.BILINEAR)
+    else:
+        if image.width > max_width:
+            height = int(round(image.height * max_width / image.width))
+            width = max_width
+            image = image.resize((width, height), resample=Image.BILINEAR)
+
+    buffer = io.BytesIO()
+    image.save(buffer, 'png')
+    base64_image = base64.b64encode(buffer.getvalue()).decode("ascii")
+    return '<img src="data:image/png;base64,{}">'.format(base64_image)
 
 
 @dataclass
--- tests/test_note.py
+++ tests/test_note.py
@@ -34,6 +34,90 @@ class TestTable:
         assert "note" in str(table)
         assert "title" in str(table)
 
+    def test_set(self):
+        table = Table(
+            columns=['a', 'b', 'c'],
+            rows=[[1, 10, 100],
+                  [2, 20, 200],
+                  [3, 30, 300]]
+        )
+        table[0, 0].value = 9
+        table[0, 'b'].value = 90
+        table[0, 'c'] = 900
+        table[0, 'c'].style.text_color = 'red'
+
+        assert table.rows[0][0] == 9
+        assert table.rows[0][1] == 90
+        assert table.rows[0][2] == 900
+
+    def test_row_operation(self):
+        table = Table(
+            columns=['a', 'b', 'c'],
+            rows=[[1, 10, 100],
+                  [3, 30, 300],
+                  [4, 40, 400]]
+        )
+        table.insert_row(1, [2, 20, 200])
+        table.append_row([5, 50, 500])
+        assert table.rows == [[1, 10, 100],
+                              [2, 20, 200],
+                              [3, 30, 300],
+                              [4, 40, 400],
+                              [5, 50, 500]]
+
+    def test_column_operation(self):
+        table = Table(
+            columns=['a', 'c', 'd'],
+            rows=[[1, 100, 1000],
+                  [2, 200, 2000],
+                  [3, 300, 3000]]
+        )
+        table.insert_column(1, 'b', [10, 20, 30])
+        table.append_column('e', [10000, 20000, 30000])
+        assert table.columns == ['a', 'b', 'c', 'd', 'e']
+        assert table.rows == [[1, 10, 100, 1000, 10000],
+                              [2, 20, 200, 2000, 20000],
+                              [3, 30, 300, 3000, 30000]]
+
+    def test_auto_add_column(self):
+        table = Table(
+            columns=['a', 'b', 'c'],
+            rows=[[1, 10, 100],
+                  [2, 20, 200],
+                  [3, 30, 300]]
+        )
+        table[0, 'comment'] = 'A comment'
+        assert table.rows[0][3] == 'A comment'
+        assert table.rows[1][3] is None
+        assert table.rows[2][3] is None
+
+    def test_repr_html(self):
+        table = Table(
+            columns=['a', 'b', 'c', 'd', 'e'],
+            rows=[[1, 10., True, 'some text', Image.new('RGB', (20, 10))],
+                  [None, None, None, None, None]],
+        )
+        html = table._repr_html_()
+        assert 'some text' in html
+
+    def test_repr_pretty(self):
+        table = Table(
+            columns=['a', 'b', 'c', 'd', 'e'],
+            rows=[[1, 10., True, 'some text', Image.new('RGB', (20, 10))],
+                  [None, None, None, None, None]],
+        )
+        text = table._repr_pretty_()
+        assert 'some text' in text
+
+    def test_repr_markdown(self):
+        table = Table(
+            columns=['a', 'b', 'c', 'd', 'e'],
+            rows=[[1, 10., True, 'some text', Image.new('RGB', (20, 10))],
+                  [None, None, None, None, None]],
+        )
+        md_text = table._repr_markdown_()
+        assert 'some text' in md_text
+
 
 class TestFigure:
 
